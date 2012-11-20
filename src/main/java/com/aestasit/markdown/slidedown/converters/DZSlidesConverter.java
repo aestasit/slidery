c6edc224f9957a3ee19080eea9faf5c3d9783b81
Andrey Adamovich <andrey@aestasit.com>
2012-11-20 22:53:53 +0100

    better template

--- src/main/java/com/aestasit/markdown/slidedown/converters/DZSlidesConverter.java
+++ src/main/java/com/aestasit/markdown/slidedown/converters/DZSlidesConverter.java
@@ -5,6 +5,7 @@ import static com.aestasit.markdown.Resources.classpath;
 import java.util.HashMap;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class DZSlidesConverter extends TextTemplateConverter {
 
@@ -24,6 +25,9 @@ public class DZSlidesConverter extends TextTemplateConverter {
 
   @Override
   protected void transformDocument(Document slidesDocument) {
+    for (Element list : slidesDocument.select("div ul, div ol")) {
+      list.addClass("incremental");
+    }
   }
 
 }
