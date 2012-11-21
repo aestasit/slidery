827b9658d284c831a77b8e6a3dbb8b62563385c1
Andrey Adamovich <andrey@aestasit.com>
2012-11-21 19:02:55 +0100

    added converter factory logic

--- src/main/java/com/aestasit/markdown/slidedown/converters/ConverterFactory.java
+++ src/main/java/com/aestasit/markdown/slidedown/converters/ConverterFactory.java
@@ -1,8 +1,40 @@
 package com.aestasit.markdown.slidedown.converters;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public final class ConverterFactory {
 
-  
-  
-  
+  public static Converter createConverter(String id) {
+    return doCreateConverter(getConverterClassName(id));
+  }
+
+  private static Converter doCreateConverter(String converterClassName) {
+    try {
+      Class<?> converterClass = Class.forName(converterClassName);
+      Object converter = converterClass.newInstance();
+      if (!(converter instanceof Converter)) {
+        converter = null;
+      }
+      return (Converter) converter;
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (InstantiationException e) {
+      throw new RuntimeException(e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static String getConverterClassName(String id) {
+    try {
+      Properties properties = new Properties();
+      properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(id + ".properties"));
+      String converterClassName = properties.getProperty("converter-class");
+      return converterClassName;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
 }
