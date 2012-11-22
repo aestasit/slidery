8fcca94d30d02472fb73794907f9fecd6da70a1b
Andrey Adamovich <andrey@aestasit.com>
2012-11-22 16:32:52 +0100

    formatting

--- src/test/java/com/aestasit/markdown/slidedown/converters/BaseConverterTest.java
+++ src/test/java/com/aestasit/markdown/slidedown/converters/BaseConverterTest.java
@@ -13,10 +13,15 @@ import com.aestasit.markdown.BaseTest;
 public class BaseConverterTest extends BaseTest {
 
   protected Configuration createConfiguration() {
-    return new SimpleConfiguration().inputFile(classpath("01_simple_slides.md"))
-        .inputFile(classpath("02_image_slides.md")).inputFile(classpath("03_code_slides.md"))
-        .inputFile(classpath("04_slide_notes.md")).staticFile(classpath("LOGO_300dpi.png"))
-        .staticFile(classpath("LOGO_FULL_300dpi.png")).outputFile(file("./tmp/presentation.html")).encoding("UTF-8");
+    return new SimpleConfiguration()
+        .inputFile(classpath("01_simple_slides.md"))
+        .inputFile(classpath("02_image_slides.md"))
+        .inputFile(classpath("03_code_slides.md"))
+        .inputFile(classpath("04_slide_notes.md"))
+        .staticFile(classpath("LOGO_300dpi.png"))
+        .staticFile(classpath("LOGO_FULL_300dpi.png"))
+        .outputFile(file("./tmp/presentation.html"))
+        .encoding("UTF-8");
   }
 
   @Before
