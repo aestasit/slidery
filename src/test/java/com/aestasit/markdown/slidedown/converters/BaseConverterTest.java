1d97c8ba71a02d59237219a1cca6d3e77e6db2b0
Andrey Adamovich <andrey@aestasit.com>
2012-11-22 16:29:23 +0100

    added cleanup at the start

--- src/test/java/com/aestasit/markdown/slidedown/converters/BaseConverterTest.java
+++ src/test/java/com/aestasit/markdown/slidedown/converters/BaseConverterTest.java
@@ -2,31 +2,27 @@ package com.aestasit.markdown.slidedown.converters;
 
 import static com.aestasit.markdown.Resources.classpath;
 import static com.aestasit.markdown.Resources.file;
-// import static org.apache.commons.io.FileUtils.deleteDirectory;
+import static org.apache.commons.io.FileUtils.deleteDirectory;
 
 import java.io.IOException;
 
-import org.junit.After;
+import org.junit.Before;
 
 import com.aestasit.markdown.BaseTest;
 
 public class BaseConverterTest extends BaseTest {
 
   protected Configuration createConfiguration() {
-    return new SimpleConfiguration()
-        .inputFile(classpath("01_simple_slides.md"))
-        .inputFile(classpath("02_image_slides.md"))
-        .inputFile(classpath("03_code_slides.md"))
-        .inputFile(classpath("04_slide_notes.md"))
-        .staticFile(classpath("LOGO_300dpi.png"))
-        .staticFile(classpath("LOGO_FULL_300dpi.png"))
-        .outputFile(file("./tmp/presentation.html"))
-        .encoding("UTF-8");
+    return new SimpleConfiguration().inputFile(classpath("01_simple_slides.md"))
+        .inputFile(classpath("02_image_slides.md")).inputFile(classpath("03_code_slides.md"))
+        .inputFile(classpath("04_slide_notes.md")).staticFile(classpath("LOGO_300dpi.png"))
+        .staticFile(classpath("LOGO_FULL_300dpi.png")).outputFile(file("./tmp/presentation.html")).encoding("UTF-8");
   }
 
-  @After
+  @Before
+  // @After
   public void clean() throws IOException {
-    // deleteDirectory(file("./tmp"));
+    deleteDirectory(file("./tmp"));
   }
 
 }
