4c2876503f9046c3fdacd2191b180ecc389492ce
Andrey Adamovich <andrey@aestasit.com>
2012-11-23 00:15:18 +0100

    first implementation of pdf converter

--- src/main/java/com/aestasit/markdown/slidedown/converters/PdfConverter.java
+++ src/main/java/com/aestasit/markdown/slidedown/converters/PdfConverter.java
@@ -1,14 +1,14 @@
 package com.aestasit.markdown.slidedown.converters;
 
 import static com.aestasit.markdown.Resources.classpath;
+import static org.apache.commons.io.FileUtils.copyFile;
+import static org.apache.commons.io.FileUtils.deleteQuietly;
+import static org.apache.commons.io.IOUtils.closeQuietly;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 
-import org.apache.commons.io.IOUtils;
-import org.jsoup.nodes.Document;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import com.lowagie.text.DocumentException;
@@ -17,35 +17,36 @@ import com.lowagie.text.DocumentException;
  * TODO: finish implemetation
  * 
  * @author Andrey Adamovich
- *
+ * 
  */
 public class PdfConverter extends TextTemplateConverter {
+
+  public static final String CONVERTER_ID = "pdf";
 
   protected void beforeStart(Configuration config) {
     config.templateFile(classpath("pdf/template.html"));
   }
 
-  protected void expandBinding(HashMap<String, Object> binding, Document slidesDocument, Configuration config) {
-  }
-
-  protected void transformDocument(Document slidesDocument) {
-  }
-
   protected void afterConversion(File inputFile, Configuration config) {
+    File tempFile = null;
     try {
       ITextRenderer renderer = new ITextRenderer();
       renderer.setDocument(config.getOutputFile());
       renderer.layout();
-      FileOutputStream outputStream = new FileOutputStream(config.getOutputFile());
+      tempFile = File.createTempFile("slidedown", ".pdf");
+      FileOutputStream outputStream = new FileOutputStream(tempFile);
       try {
-        renderer.createPDF(outputStream);
+        renderer.createPDF(outputStream, true);
       } catch (DocumentException e) {
         throw new RuntimeException(e);
       } finally {
-        IOUtils.closeQuietly(outputStream); 
+        closeQuietly(outputStream);
       }
+      copyFile(tempFile, config.getOutputFile());
     } catch (IOException e) {
       throw new RuntimeException(e);
+    } finally {
+      deleteQuietly(tempFile);
     }
   }
 
